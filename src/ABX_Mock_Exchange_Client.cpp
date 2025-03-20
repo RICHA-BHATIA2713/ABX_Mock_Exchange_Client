d0b70a2f9f23ac13700dc50680b43c375c1dfdad
Richa Bhatia <richu@Richas-MacBook-Air.local>
2025-03-20 18:36:57 +0530

    Request All Feed Packet

--- src/ABX_Mock_Exchange_Client.cpp
+++ src/ABX_Mock_Exchange_Client.cpp
@@ -17,6 +17,24 @@ int main() {
         return EXIT_FAILURE;
     }
 
+
+    //1. Request All Feed Packets.
+    Request all_packet_request((int)RequestPayloadFormat::callType);
+	if (GetABXClient()->send_request(all_packet_request))
+	{
+		cout << "Send Request done: type " << (int)RequestPayloadFormat::callType << endl;
+		int recv_bytes = INT_MAX;
+		while(recv_bytes > 0)
+		{
+			recv_bytes = GetABXClient()->handle_response(GetSeqVector());
+		}
+	}
+	else
+	{
+		cerr << "Send Request Not done: type " << (int)RequestPayloadFormat::callType << endl;
+	}
+
+
     DestroyGlobalMembers();
 
 	return EXIT_SUCCESS;
