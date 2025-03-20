4ea6f461cb86b3999615fb7cd4d62fa60d71a992
Richa Bhatia <richu@Richas-MacBook-Air.local>
2025-03-20 18:42:58 +0530

    Request missing sequences functionality added

--- src/ABX_Mock_Exchange_Client.cpp
+++ src/ABX_Mock_Exchange_Client.cpp
@@ -36,6 +36,36 @@ int main() {
 
 	PrintSeqVector();
 
+	//Connection closed create connection again
+    if (!GetABXClient()->create_connection())
+    {
+        return EXIT_FAILURE;
+    }
+
+    //2. Request Missing Sequence Packets.
+    int seq_counter = 1, idx_counter = 0, last_counter = GetSeqVector().back();
+    while(idx_counter < GetSeqVector().size() && seq_counter<last_counter)
+    {
+    	if(GetSeqVector()[idx_counter] == seq_counter) {
+    		idx_counter++;
+    		seq_counter++;
+    	}
+    	else if(GetSeqVector()[idx_counter] > seq_counter) {
+    		Request resend_request((int)RequestPayloadFormat::resendSeq, seq_counter);
+    		if (GetABXClient()->send_request(resend_request))
+    		{
+    			cout << "Send Request done: type " << (int)RequestPayloadFormat::resendSeq << endl;
+    			GetABXClient()->handle_response(GetSeqVector());
+    		}
+    		else
+    		{
+    			cerr << "Send Request Not done: type " << (int)RequestPayloadFormat::resendSeq << endl;
+    			return EXIT_FAILURE;
+    		}
+    		seq_counter++;
+    	}
+    }
+
     DestroyGlobalMembers();
 
 	return EXIT_SUCCESS;
